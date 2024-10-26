d79e79594dd099c50388b7e8047395f55754dc31
Josef Melcr <melcrjos@fit.cvut.cz>
2024-10-26 22:13:59 +0200

    image: make Image covariant

--- src/main/scala/image/Image.scala
+++ src/main/scala/image/Image.scala
@@ -3,4 +3,4 @@ package image;
 import image.ImageData;
 import image.pixel.Pixel
 
-class Image[T <: Pixel](val data : Vector[Vector[T]]) {}
+class Image[+T <: Pixel](val data : Vector[Vector[T]]) {}
